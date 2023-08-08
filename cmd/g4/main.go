e16bcff8cd8db549921cbeabf281b4cff0c27186
Pierre-Louis Nordmann <pierrelouis.nordmann@gmail.com>
2023-08-08 21:52:50 +0000

    feat: added modal and various minor changes

--- cmd/g4/main.go
+++ cmd/g4/main.go
@@ -18,8 +18,11 @@ func main() {
 			spec:       spec,
 			game:       bitsim.Game{Board: board, Mover: g4.Yellow},
 			keyHandler: KeyHandler{keyMap: defaultKeymap},
+			gameStatus: inProgress,
+			connStatus: connecting,
 		},
 		tea.WithAltScreen(),
+		tea.WithMouseAllMotion(),
 	)
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
